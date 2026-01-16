1b37ecfc860f833909620a37add02b014b1660fe
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 11:17:32 +0530

    created the router files for order py file

--- backend/routers/order.py
+++ backend/routers/order.py
@@ -5,3 +5,24 @@ from schemas.order import OrderCreate, OrderItemCreate, InvoiceResponse
 from core import crud
 
 router = APIRouter(prefix="/orders", tags=["Orders"])
+
+@router.post("/")
+def create_order(
+    data: OrderCreate,
+    db: Session = Depends(get_db)
+):
+    order = crud.create_order(
+        db,
+        data.table_number,
+        data.created_by
+    )
+    return {"order_id": order.id, "status": "Order created"}
+
+@router.post("/{order_id}/items")
+def add_item(
+    order_id: int,
+    item: OrderItemCreate,
+    db: Session = Depends(get_db)
+):
+    crud.add_order_item(db, order_id, item)
+    return {"message": "Item added to order"}
