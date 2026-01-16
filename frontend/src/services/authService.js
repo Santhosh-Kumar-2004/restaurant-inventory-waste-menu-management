ce7e756b8c072d109af21d644f4dfa25a05f527c
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 14:50:46 +0530

    Updated the backend url tomatch them

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,14 +1,14 @@
 import { apiFetch } from "../api/http";
 
 export function loginUser(data) {
-  return apiFetch("/login", {
+  return apiFetch("/users/login", {
     method: "POST",
     body: JSON.stringify(data)
   });
 }
 
 export function createUser(data) {
-  return apiFetch("/users", {
+  return apiFetch("/users/users", {
     method: "POST",
     body: JSON.stringify(data)
   });
