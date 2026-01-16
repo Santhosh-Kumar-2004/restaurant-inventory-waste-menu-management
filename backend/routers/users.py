e62f303f95d228075153713205f16f1a185ce04e
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-16 10:31:26 +0530

    Updated the suer.py

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -2,8 +2,12 @@ from schemas.schema import UserCreate, UserLogin, UserResponse, RoleUpdate
 import models
 from core.crud import create_user
 from sqlalchemy.orm import Session
+from fastapi import Depends, APIRouter
+from core.database import get_db
 
-@app.post("/users", response_model=UserResponse)
+router = APIRouter(prefix="/users")
+
+@router.post("/users", response_model=UserResponse)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(
         db,
